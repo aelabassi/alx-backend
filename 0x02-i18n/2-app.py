b2a148a25d807effe004f7f935b946c9a3a89db9
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-05 12:38:09 +0100

    basic flask  #2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Basic Flask app, Basic Babel setup, Get locale from request,"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Babel configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Get locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 1-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
