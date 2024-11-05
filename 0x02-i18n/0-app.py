1703d8ed9d7f393cc9e185873eeec75b72fb36b9
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-05 12:08:55 +0100

    basic flask  #1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ def index() -> str:
     """ GET /
     Return: 1-index.html
     """
-    return render_template('1-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
