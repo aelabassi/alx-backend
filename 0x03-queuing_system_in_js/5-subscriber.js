e22447ac351cc7858d9bc796aadd74d85f796677
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-12-02 15:49:54 +0100

    Node redis client publisher and subscriber #5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,20 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'EXIT_MSG') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
