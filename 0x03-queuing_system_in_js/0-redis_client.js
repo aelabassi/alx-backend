66fd5385d8742c51168f08c948a8d4db59b0f893
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-12-02 13:08:25 +0100

    Node redis basic operations #2

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,8 +3,8 @@ import redis from 'redis';
 const client = redis.createClient();
 
 client.on('connect', () => {
-    console.log('Redis client connected to the server');
-})
+  console.log('Redis client connected to the server');
+});
 client.on('error', (error) => {
-    console.error(`Redis client not connected to the server: ${error.message}`);
-})
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
